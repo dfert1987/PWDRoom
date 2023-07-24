12413a001157f0e7575a2681d2ff6c51a7ca3911
David Fertitta <Davidj.fertitta@gmail.com>
2023-07-24 16:54:09 -0600

    pages in place and esl lint error fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
     return (
         <>
-            <h1 className='text-2xl'>Hello World!</h1>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </Router>
         </>
     );
 }
---
+++ src/pages/Bathroom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Bathroom() {
+  return (
+    <div>
+      Bathroom
+    </div>
+  )
+}
